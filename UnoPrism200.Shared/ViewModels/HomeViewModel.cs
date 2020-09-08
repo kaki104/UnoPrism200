c530b1120dc1a443071057c57c86a72c77fa3a5e
Connor Park <kaki104@hanmail.net>
2020-09-08 16:59:23 +0900

    Step 1 Add NavigationView to Shell

---
+++ UnoPrism200.Shared/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnoPrism200.ViewModels;
+
+namespace UnoPrism200.Shared.ViewModels
+{
+    public class HomeViewModel : ViewModelBase
+    {
+    }
+}
